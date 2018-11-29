7953a5bd6af118b521744cd5ed6b5d4699e2e8b4
Robert Wolff <ro.wolff@gmx.net>
2018-11-29 17:35:08 +0100

    Add search back

--- src/PlaceList.bs.js
+++ src/PlaceList.bs.js
@@ -2,13 +2,17 @@
 'use strict';
 
 var $$Array = require("bs-platform/lib/js/array.js");
+var Block = require("bs-platform/lib/js/block.js");
+var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
+var $$String = require("bs-platform/lib/js/string.js");
+var Belt_List = require("bs-platform/lib/js/belt_List.js");
 var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
 var Data$ReactTemplate = require("./Data.bs.js");
 var Phone$ReactTemplate = require("./Phone.bs.js");
 
-var component = ReasonReact.statelessComponent("PlaceList-ReactTemplate");
+var component = ReasonReact.reducerComponent("PlaceList-ReactTemplate");
 
 function s(prim) {
   return prim;
@@ -25,8 +29,16 @@ function make(_children) {
           /* willUnmount */component[/* willUnmount */6],
           /* willUpdate */component[/* willUpdate */7],
           /* shouldUpdate */component[/* shouldUpdate */8],
-          /* render */(function (_self) {
-              return React.createElement("div", undefined, React.createElement("ul", undefined, Belt_Array.map($$Array.of_list(Data$ReactTemplate.places), (function (place) {
+          /* render */(function (self) {
+              var filteredPlaces = Belt_List.keep(Data$ReactTemplate.places, (function (place) {
+                      return $$String.lowercase(place[/* name */1]).includes($$String.lowercase(self[/* state */1][/* term */0]));
+                    }));
+              return React.createElement("div", undefined, React.createElement("input", {
+                              placeholder: "search here",
+                              onChange: (function ($$event) {
+                                  return Curry._1(self[/* send */3], /* Search */[$$event.target.value]);
+                                })
+                            }), React.createElement("ul", undefined, Belt_Array.map($$Array.of_list(filteredPlaces), (function (place) {
                                     var match = place[/* image */3][/* attributionUrl */2];
                                     return React.createElement("li", {
                                                 key: place[/* id */0]
@@ -38,9 +50,17 @@ function make(_children) {
                                                     }, place[/* image */3][/* attribution */1]) : React.createElement("p", undefined, place[/* image */3][/* attribution */1]), ReasonReact.element(undefined, undefined, Phone$ReactTemplate.make(place[/* phone */5], /* array */[])));
                                   }))));
             }),
-          /* initialState */component[/* initialState */10],
+          /* initialState */(function (param) {
+              return /* record */[/* term */""];
+            }),
           /* retainedProps */component[/* retainedProps */11],
-          /* reducer */component[/* reducer */12],
+          /* reducer */(function (action, _state) {
+              if (action) {
+                return /* Update */Block.__(0, [/* record */[/* term */action[0]]]);
+              } else {
+                return /* Update */Block.__(0, [/* record */[/* term */""]]);
+              }
+            }),
           /* jsElementWrapped */component[/* jsElementWrapped */13]
         ];
 }
--- src/PlaceList.re
+++ src/PlaceList.re
@@ -1,14 +1,40 @@
-let component = ReasonReact.statelessComponent(__MODULE__);
+type state = {term: string};
+
+type action =
+  | Search(string)
+  | Reset;
+
+let component = ReasonReact.reducerComponent(__MODULE__);
 
 let s = ReasonReact.string;
 
 let make = _children => {
   ...component,
-  render: _self =>
+  initialState: () => {term: ""},
+  reducer: (action, _state) =>
+    switch (action) {
+    | Search(text) => ReasonReact.Update({term: text})
+    | Reset => ReasonReact.Update({term: ""})
+    },
+
+  render: self => {
+    let filteredPlaces =
+      Data.places->Belt.List.keep(place =>
+        Js.String.includes(
+          self.state.term->String.lowercase,
+          place.name->String.lowercase,
+        )
+      );
     <div>
+      <input
+        onChange={
+          event => self.send(Search(ReactEvent.Form.target(event)##value))
+        }
+        placeholder="search here"
+      />
       <ul>
         {
-          Data.places
+          filteredPlaces
           ->Array.of_list
           ->Belt.Array.map(place =>
               <li key={place.id}>
@@ -26,5 +52,6 @@ let make = _children => {
           ->ReasonReact.array
         }
       </ul>
-    </div>,
+    </div>;
+  },
 };
